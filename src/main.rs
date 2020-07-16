6cebaebc594586651ec7fae4bb1c3880d04ff0da
boxdot <d@zerovolt.org>
2020-07-16 12:23:59 +0200

    Comments and MaybeUninit.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ use glutin::{
     ContextBuilder, PossiblyCurrent,
 };
 
+use gl::types::*;
+
+use std::mem;
+use std::mem::MaybeUninit;
+use std::ptr;
+
 pub mod gl {
     #![allow(clippy::all)]
     include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
@@ -48,19 +54,25 @@ fn main() {
 struct Context {
     gl: gl::Gl,
     /// scene rendering
-    program: gl::types::GLuint,
+    program: GLuint,
     /// blur rendering
-    blur_program: gl::types::GLuint,
-    tex: [gl::types::GLuint; 2],
-    fb: [gl::types::GLuint; 2],
+    blur_program: GLuint,
+    /// texture for the rendered scene and the first blur pass
+    tex: [GLuint; 2],
+    /// framebuffers for the rendered scene and the first blur pass
+    fb: [GLuint; 2],
     /// scene objects
-    scene_vao: gl::types::GLuint,
+    scene_vao: GLuint,
     /// screen (blur) objects
-    screen_vao: gl::types::GLuint,
-    texture_uniform: gl::types::GLint,
-    texture_size_uniform: gl::types::GLint,
-    sigma_uniform: gl::types::GLint,
-    dir_uniform: gl::types::GLint,
+    screen_vao: GLuint,
+    /// texture blur shader parameter
+    texture_uniform: GLint,
+    /// texture size blur shader parameter
+    texture_size_uniform: GLint,
+    /// Gauss blur shader parameter
+    sigma_uniform: GLint,
+    /// blur direction parameter
+    dir_uniform: GLint,
 }
 
 impl Context {
@@ -85,44 +97,45 @@ impl Context {
             gl.AttachShader(program, vs);
             gl.AttachShader(program, fs);
             gl.LinkProgram(program);
-            gl.UseProgram(program);
 
             let pos_attrib = gl.GetAttribLocation(program, b"position\0".as_ptr() as *const _);
             let color_attrib = gl.GetAttribLocation(program, b"color\0".as_ptr() as *const _);
 
-            let mut tri_buf = std::mem::zeroed();
-            gl.GenBuffers(1, &mut tri_buf);
+            let mut tri_buf = MaybeUninit::zeroed();
+            gl.GenBuffers(1, tri_buf.as_mut_ptr());
+            let tri_buf = tri_buf.assume_init();
             gl.BindBuffer(gl::ARRAY_BUFFER, tri_buf);
             gl.BufferData(
                 gl::ARRAY_BUFFER,
-                (VERTEX_DATA.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+                (VERTEX_DATA.len() * mem::size_of::<f32>()) as GLsizeiptr,
                 VERTEX_DATA.as_ptr() as *const _,
                 gl::STATIC_DRAW,
             );
 
             let scene_vao = {
-                let mut vao = std::mem::zeroed();
-                gl.GenVertexArrays(1, &mut vao);
+                let mut vao = MaybeUninit::zeroed();
+                gl.GenVertexArrays(1, vao.as_mut_ptr());
+                let vao = vao.assume_init();
                 gl.BindVertexArray(vao);
 
                 gl.VertexAttribPointer(
-                    pos_attrib as gl::types::GLuint,
-                    2,                                                    // size
-                    gl::FLOAT,                                            // type
-                    gl::FALSE,                                            // normalized
-                    5 * std::mem::size_of::<f32>() as gl::types::GLsizei, // stride
-                    std::ptr::null(),                                     // pointer
+                    pos_attrib as GLuint,
+                    2,                                    // size
+                    gl::FLOAT,                            // type
+                    gl::FALSE,                            // normalized
+                    5 * mem::size_of::<f32>() as GLsizei, // stride
+                    ptr::null(),                          // pointer
                 );
                 gl.VertexAttribPointer(
-                    color_attrib as gl::types::GLuint,
+                    color_attrib as GLuint,
                     3,
                     gl::FLOAT,
                     gl::FALSE,
-                    5 * std::mem::size_of::<f32>() as gl::types::GLsizei,
-                    (2 * std::mem::size_of::<f32>()) as *const () as *const _,
+                    5 * mem::size_of::<f32>() as GLsizei,
+                    (2 * mem::size_of::<f32>()) as *const _,
                 );
-                gl.EnableVertexAttribArray(pos_attrib as gl::types::GLuint);
-                gl.EnableVertexAttribArray(color_attrib as gl::types::GLuint);
+                gl.EnableVertexAttribArray(pos_attrib as GLuint);
+                gl.EnableVertexAttribArray(color_attrib as GLuint);
 
                 gl.BindVertexArray(0);
                 vao
@@ -133,7 +146,8 @@ impl Context {
             gl.AttachShader(blur_program, blur_fs);
             gl.LinkProgram(blur_program);
 
-            let in_pos_attrib = gl.GetAttribLocation(blur_program, b"inPos\0".as_ptr() as *const _);
+            let position_attrib =
+                gl.GetAttribLocation(blur_program, b"position\0".as_ptr() as *const _);
             let texture_uniform =
                 gl.GetUniformLocation(blur_program, b"u_texture\0".as_ptr() as *const _);
             let texture_size_uniform =
@@ -142,45 +156,49 @@ impl Context {
                 gl.GetUniformLocation(blur_program, b"u_sigma\0".as_ptr() as *const _);
             let dir_uniform = gl.GetUniformLocation(blur_program, b"u_dir\0".as_ptr() as *const _);
 
-            let mut quad_buf = std::mem::zeroed();
-            gl.GenBuffers(1, &mut quad_buf);
+            let mut quad_buf = MaybeUninit::zeroed();
+            gl.GenBuffers(1, quad_buf.as_mut_ptr());
+            let quad_buf = quad_buf.assume_init();
             gl.BindBuffer(gl::ARRAY_BUFFER, quad_buf);
             gl.BufferData(
                 gl::ARRAY_BUFFER,
-                (SCREEN_RECT2.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+                (SCREEN_RECT2.len() * mem::size_of::<f32>()) as GLsizeiptr,
                 SCREEN_RECT2.as_ptr() as *const _,
                 gl::STATIC_DRAW,
             );
 
             // screen vao
             let screen_vao = {
-                let mut vao = std::mem::zeroed();
-                gl.GenVertexArrays(1, &mut vao);
+                let mut vao = MaybeUninit::zeroed();
+                gl.GenVertexArrays(1, vao.as_mut_ptr());
+                let vao = vao.assume_init();
                 gl.BindVertexArray(vao);
 
-                gl.EnableVertexAttribArray(in_pos_attrib as gl::types::GLuint);
+                gl.EnableVertexAttribArray(position_attrib as GLuint);
                 gl.VertexAttribPointer(
-                    in_pos_attrib as gl::types::GLuint,
+                    position_attrib as GLuint,
                     2,
                     gl::FLOAT,
                     gl::FALSE,
                     0,
-                    std::ptr::null(),
+                    ptr::null(),
                 );
                 gl.BindVertexArray(0);
                 vao
             };
 
-            let mut viewport = [0, 0, 0, 0];
-            gl.GetIntegerv(gl::VIEWPORT, viewport.as_mut_ptr());
-            let width = viewport[2];
-            let height = viewport[3];
+            // Note: viewport size is not necessarily the same as window size
+            let (width, height) = get_viewport_size(&gl);
 
-            let mut fb = [std::mem::zeroed(), std::mem::zeroed()];
-            let mut tex = [std::mem::zeroed(), std::mem::zeroed()];
+            // generate framebuffers for rendering into texture
+            let mut fb = [MaybeUninit::<GLuint>::zeroed(); 2];
+            let mut tex = [MaybeUninit::<GLuint>::zeroed(); 2];
 
-            gl.GenFramebuffers(2, fb.as_mut_ptr());
-            gl.GenTextures(2, tex.as_mut_ptr());
+            gl.GenFramebuffers(2, fb.as_mut_ptr() as *mut _);
+            gl.GenTextures(2, tex.as_mut_ptr() as *mut _);
+
+            let fb = mem::transmute::<_, [GLuint; 2]>(fb);
+            let tex = mem::transmute::<_, [GLuint; 2]>(tex);
 
             for i in 0..2 {
                 gl.BindFramebuffer(gl::FRAMEBUFFER, fb[i]);
@@ -188,24 +206,16 @@ impl Context {
                 gl.TexImage2D(
                     gl::TEXTURE_2D,
                     0,
-                    gl::RGBA as gl::types::GLint,
-                    width,
-                    height,
+                    gl::RGBA as GLint,
+                    width as GLint,
+                    height as GLint,
                     0,
                     gl::RGBA,
                     gl::UNSIGNED_BYTE,
-                    std::ptr::null(),
+                    ptr::null(),
                 );
-                gl.TexParameteri(
-                    gl::TEXTURE_2D,
-                    gl::TEXTURE_MIN_FILTER,
-                    gl::LINEAR as gl::types::GLint,
-                );
-                gl.TexParameteri(
-                    gl::TEXTURE_2D,
-                    gl::TEXTURE_MAG_FILTER,
-                    gl::LINEAR as gl::types::GLint,
-                );
+                gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as GLint);
+                gl.TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as GLint);
                 gl.BindTexture(gl::TEXTURE_2D, 0);
 
                 gl.FramebufferTexture2D(
@@ -216,14 +226,20 @@ impl Context {
                     0,
                 );
 
-                let mut renderbuffer = std::mem::zeroed();
-                gl.GenFramebuffers(1, &mut renderbuffer);
+                let mut renderbuffer = MaybeUninit::zeroed();
+                gl.GenFramebuffers(1, renderbuffer.as_mut_ptr());
+                let renderbuffer = renderbuffer.assume_init();
                 gl.BindRenderbuffer(gl::RENDERBUFFER, renderbuffer);
-                gl.RenderbufferStorage(gl::RENDERBUFFER, gl::DEPTH24_STENCIL8, width, height);
+                gl.RenderbufferStorage(
+                    gl::RENDERBUFFER,
+                    gl::DEPTH,
+                    width as GLint,
+                    height as GLint,
+                );
                 gl.BindRenderbuffer(gl::RENDERBUFFER, 0);
                 gl.FramebufferRenderbuffer(
                     gl::FRAMEBUFFER,
-                    gl::DEPTH_STENCIL_ATTACHMENT,
+                    gl::DEPTH_ATTACHMENT,
                     gl::RENDERBUFFER,
                     renderbuffer,
                 );
@@ -255,7 +271,7 @@ impl Context {
         // draw scene
         gl.BindFramebuffer(gl::FRAMEBUFFER, self.fb[0]);
         gl.ClearColor(color[0], color[1], color[2], color[3]);
-        gl.Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+        gl.Clear(gl::COLOR_BUFFER_BIT);
 
         gl.UseProgram(self.program);
         gl.BindVertexArray(self.scene_vao);
@@ -268,14 +284,9 @@ impl Context {
 
         gl.UseProgram(self.blur_program);
         gl.BindVertexArray(self.screen_vao);
-        gl.Disable(gl::DEPTH_TEST);
         gl.BindTexture(gl::TEXTURE_2D, self.tex[0]);
 
-        let mut viewport = [0f32; 4];
-        gl.GetFloatv(gl::VIEWPORT, viewport.as_mut_ptr());
-        let width = viewport[2];
-        let height = viewport[3];
-
+        let (width, height) = get_viewport_size(gl);
         gl.Uniform1i(self.texture_uniform, 0);
         gl.Uniform2f(self.texture_size_uniform, width, height);
         gl.Uniform1f(self.sigma_uniform, 0.5);
@@ -299,19 +310,10 @@ impl Context {
     }
 }
 
-fn create_shader(
-    gl: &gl::Gl,
-    kind: gl::types::GLuint,
-    source: &[u8],
-) -> Result<gl::types::GLuint, String> {
+fn create_shader(gl: &gl::Gl, kind: GLuint, source: &[u8]) -> Result<GLuint, String> {
     unsafe {
         let id = gl.CreateShader(kind);
-        gl.ShaderSource(
-            id,
-            1,
-            [source.as_ptr() as *const _].as_ptr(),
-            std::ptr::null(),
-        );
+        gl.ShaderSource(id, 1, [source.as_ptr() as *const _].as_ptr(), ptr::null());
         gl.CompileShader(id);
 
         let mut success = 1;
@@ -321,7 +323,7 @@ fn create_shader(
             gl.GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
 
             let mut buffer: Vec<u8> = Vec::with_capacity(len as usize);
-            gl.GetShaderInfoLog(id, len, std::ptr::null_mut(), buffer.as_mut_ptr() as *mut _);
+            gl.GetShaderInfoLog(id, len, ptr::null_mut(), buffer.as_mut_ptr() as *mut _);
             buffer.set_len(len as usize);
             Err(String::from_utf8_unchecked(buffer))
         } else {
@@ -330,13 +332,22 @@ fn create_shader(
     }
 }
 
+unsafe fn get_viewport_size(gl: &gl::Gl) -> (f32, f32) {
+    let mut viewport = [0f32; 4];
+    gl.GetFloatv(gl::VIEWPORT, viewport.as_mut_ptr());
+    (viewport[2], viewport[3])
+}
+
+/// Triangle 2D vertices and colors
 #[rustfmt::skip]
 static VERTEX_DATA: [f32; 15] = [
+    // vertex    // color
     -0.5, -0.5,  1.0,  0.0,  0.0,
      0.0,  0.5,  0.0,  1.0,  0.0,
      0.5, -0.5,  0.0,  0.0,  1.0,
 ];
 
+/// 2D screen rectange
 #[rustfmt::skip]
 const SCREEN_RECT2: [f32; 8] = [
     -1.0, -1.0,
@@ -345,6 +356,7 @@ const SCREEN_RECT2: [f32; 8] = [
     1.0, 1.0,
 ];
 
+/// Scene vector shader
 const VS_SRC: &[u8] = b"
 #version 100
 precision mediump float;
@@ -360,6 +372,7 @@ void main() {
 }
 \0";
 
+/// Scene fragment shader
 const FS_SRC: &[u8] = b"
 #version 100
 precision mediump float;
@@ -371,25 +384,29 @@ void main() {
 }
 \0";
 
-// source: https://stackoverflow.com/questions/44779142/opengl-es-2-0-gaussian-blur-on-triangle
+/// Blur vector shader
+///
+/// source: https://stackoverflow.com/questions/44779142/opengl-es-2-0-gaussian-blur-on-triangle
 const BLUR_VS_SRC: &[u8] = b"
 #version 100
 precision mediump float;
 
-attribute vec2 inPos;
-varying   vec2 pos;
+attribute vec2 position;
+varying   vec2 v_position;
 
 void main() {
-    pos = inPos;
-    gl_Position = vec4( inPos, 0.0, 1.0 );
+    v_position = position;
+    gl_Position = vec4( position, 0.0, 1.0 );
 }
 \0";
 
-// source: https://stackoverflow.com/questions/44779142/opengl-es-2-0-gaussian-blur-on-triangle
+/// Blur fragment shader
+///
+/// source: https://stackoverflow.com/questions/44779142/opengl-es-2-0-gaussian-blur-on-triangle
 const BLUR_FS_SRC: &[u8] = b"
 #version 100
 precision mediump float;
-varying vec2 pos;
+varying vec2 v_position;
 
 uniform sampler2D u_texture;
 uniform vec2      u_textureSize;
@@ -405,7 +422,7 @@ float CalcGauss( float x, float sigma )
 
 void main()
 {
-    vec2 texC     = pos.st * 0.5 + 0.5;
+    vec2 texC     = v_position.st * 0.5 + 0.5;
     vec4 texCol   = texture2D( u_texture, texC );
     vec4 gaussCol = vec4( texCol.rgb, 1.0 );
     vec2 step     = u_dir / u_textureSize;
